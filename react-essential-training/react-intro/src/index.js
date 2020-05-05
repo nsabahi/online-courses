14118546b6bcfdb5f8a725a167b4707d3bcc9d91
nsabahi <nader.sabahi12@gmail.com>
2020-05-05 13:03:39 -0400

    Add state handling and conditional rendering

--- react-essential-training/react-intro/src/index.js
+++ react-essential-training/react-intro/src/index.js
@@ -7,28 +7,59 @@ let bookList = [
   {title: "Cat's Cradle", author: "Kurt Vonnegut", pages: 304}
 ]
 
-const Book = ({title, author, pages}) => {
+const Book = ({title, author, pages, freeBookmark}) => {
   return(
     <section>
       <h2>{title}</h2>
       <p>by: {author}</p>
       <p>Pages: {pages} pages</p>
+      <p>Free bookmark today: {freeBookmark ? 'yes!' : 'no!'} </p>
     </section>
   )
 }
 
-const Library = ({books}) => {
-  return (
-    <div>
-      {books.map(
-        (book, i) => <Book 
-          key={i}
-          title={book.title}
-          author={book.author}
-          pages={book.pages}/>
-      )}
-    </div>
-  )
+const Hiring = () =>
+  <div>
+    <p>The Library is hiring! Visit www.thelibrary.com/hiring for more.</p>
+  </div>
+
+const NotHiring = () =>
+  <div>
+    <p>The Library is not hiring. Check back for more details.</p>
+  </div>
+
+class Library extends Component {
+  state = { 
+    open: true,
+    freeBookmark: true,
+    hiring: true,
+  }
+
+  // Arrow functions automatically bind 'this'
+  toggleOpenClosed = () => {
+    this.setState( prevState => ({
+      open:!prevState.open
+    }))
+  }
+
+  render() {
+    const { books } = this.props
+    return (
+      <div>
+        {this.state.hiring ? <Hiring /> : <NotHiring />}
+        <h1>The library is: {this.state.open ? 'Open' : 'Closed'}</h1>
+        <button onClick={this.toggleOpenClosed}>Change</button>
+        {books.map(
+          (book, i) => <Book 
+            key={i}
+            title={book.title}
+            author={book.author}
+            pages={book.pages}
+            freeBookmark={this.state.freeBookmark}/>
+        )}
+      </div>
+    )
+  }
 }
 
 render(
