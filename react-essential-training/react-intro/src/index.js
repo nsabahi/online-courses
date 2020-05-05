77db14b48093809dc51604a98be1465503b4d231
nsabahi <nader.sabahi12@gmail.com>
2020-05-04 21:34:53 -0400

    Use Props to display data

--- react-essential-training/react-intro/src/index.js
+++ react-essential-training/react-intro/src/index.js
@@ -1,16 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var style = {
-  backgroundColor: 'orange',
-  color: 'white',
-  fontFamily: 'Arial',
+let exerciseData = {
+  total: 50,
+  biking: 10,
+  basketball: 15,
+  workout: 10,
+  goal: 100,
+}
+
+class ExerciseDayCounter extends React.Component {
+  render() {
+    return (
+      <section>
+        <div>
+          <p>Total: {this.props.total}</p>
+        </div>
+        <div>
+          <p>Biking: {this.props.biking}</p>
+        </div>
+        <div>
+          <p>Basketball: {this.props.basketball}</p>
+        </div>
+        <div>
+          <p>Workout: {this.props.workout}</p>
+        </div>
+        <div>
+          <p>Goal: {this.props.goal}</p>
+        </div>
+      </section>
+    )
+  }
 }
 
 ReactDOM.render(
-  <div style={style}>
-    <h1 id='heading-element'>Hello World!</h1>
-    <p>Thanks for stopping by.</p>
-  </div>,
+  <ExerciseDayCounter 
+    total={exerciseData.total}
+    biking={exerciseData.biking}
+    basketball={exerciseData.basketball}
+    workout={exerciseData.workout}
+    goal={exerciseData.goal}/>,
   document.getElementById('root')
 )
