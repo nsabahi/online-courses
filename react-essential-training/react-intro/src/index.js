d9eae779c82d7a9d53ad9d32e21bb5a3d90a63c8
nsabahi <nader.sabahi12@gmail.com>
2020-05-04 20:28:09 -0400

    Render React elements with JSX

--- react-essential-training/react-intro/src/index.js
+++ react-essential-training/react-intro/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'Arial',
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div style={style}>
+    <h1 id='heading-element'>Hello World!</h1>
+    <p>Thanks for stopping by.</p>
+  </div>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
