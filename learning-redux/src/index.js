fd75359b3b7ba754de779b9100bc47b07da91f2e
nsabahi <nader.sabahi12@gmail.com>
2020-05-07 14:00:11 -0400

    create array and object reducers for skidays

--- learning-redux/src/index.js
+++ learning-redux/src/index.js
@@ -1,16 +1,29 @@
 import C from "./constants";
-import { goal } from "./store/reducers";
+import { allSkiDays } from "./store/reducers";
 
-const state = 10;
+const state = [
+  {
+    resort: "Kirkwood",
+    date: "2016-12-15",
+    powder: true,
+    backcountry: false,
+  },
+];
+
 const action = {
-  type: C.SET_GOAL,
-  payload: 15,
+  type: C.ADD_DAY,
+  payload: {
+    resort: "Boreal",
+    date: "2016-12-16",
+    powder: false,
+    backcountry: false,
+  },
 };
 
-const nextState = goal(state, action);
+const nextState = allSkiDays(state, action);
 
 console.log(`
-    initial goal: ${state}
+    initial state: ${JSON.stringify(state)}
     action: ${JSON.stringify(action)}
-    new goal: ${nextState}
+    new state: ${JSON.stringify(nextState)}
 `);
--- learning-redux/src/store/reducers.js
+++ learning-redux/src/store/reducers.js
@@ -1,9 +1,34 @@
 import C from "../constants";
 
-export const goal = (state = 10, action) => {
-  if (action.type === C.SET_GOAL) {
-    return parseInt(action.payload);
-  } else {
-    return state;
+export const goal = (state = 10, action) =>
+  action.type === C.SET_GOAL ? parseInt(action.payload) : state;
+
+export const skiDay = (state = null, action) =>
+  action.type === C.ADD_DAY ? action.payload : state;
+
+export const errors = (state = [], action) => {
+  switch (action.type) {
+    case C.ADD_ERROR:
+      return [...state, action.payload];
+    case C.CLEAR_ERROR:
+      return state.filter((message, i) => i != action.payload);
+    default:
+      return state;
+  }
+};
+
+export const allSkiDays = (state = [], action) => {
+  switch (action.type) {
+    case C.ADD_DAY:
+      const hasDayAlread = state.some(
+        (skiDay) => skiDay.date === action.payload.date
+      );
+
+      return hasDayAlread ? state : [...state, skiDay(null, action)];
+
+    case C.REMOVE_DAY:
+      return state.filter((skiDay) => skiDay.date != action.payload);
+    default:
+      return state;
   }
 };
